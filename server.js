9d34741c77b689d76358e0c4650091e162b6705e
Harshdeep Singh <harshdeepsingh13@gmail.com>
2023-04-22 14:32:57 -0700

    updates for deployment

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const morgan = require('morgan');
 const logger = require("./services/logger");
 const mongooseConnection = require('./services/mongooseConnection');
 const v1Routes = require('./api/v1');
-const errorMiddleware = require('./middlewares/errorMIddleware');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 const path = require('path');
 
 //init
